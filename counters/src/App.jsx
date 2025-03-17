7f70acd5e79972a6449c499e4c0d80b4d3699822
Viviancat666 <vivian9069@gmail.com>
2025-03-17 20:38:50 +0200

    Create App.jsx

---
+++ counters/src/App.jsx
@@ -0,0 +1,15 @@
+
+import "./App.css";
+import Calculator from "./components/Calculator";
+import Counter from "./components/Counter";
+
+function App() {
+    return(
+        <>
+        <Counter/>
+        <Calculator/>
+        </>
+    );
+}
+
+export default App;
